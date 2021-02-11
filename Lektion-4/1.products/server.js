7b25571ec4f251d9acd16af39f2929d386f101bd
Joachim Karlsson <joachim.karlsson@utb.ecutbildning.se>
2021-02-11 09:45:55 +0100

    lektion 4

---
+++ Lektion-4/1.products/server.js
@@ -0,0 +1,17 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 9999;
+
+const serverURI = 'http://localhost:' + PORT;
+const mongoURI = 'mongodb+srv://Joachim:TonyHawk1@lektion3.znsoi.mongodb.net/REST?retryWrites=true&w=majority'
+
+
+app.listen(PORT, () => console.log('Webserver: ' + serverURI));
+
+mongoose
+.set('useCreateIndex', true)
+.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log('Conected to Data Base!'));
