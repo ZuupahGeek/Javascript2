ec7d4ed78b3d50d5367c1571bb8ca031d72d58f0
Joachim Karlsson <joachim.karlsson@utb.ecutbildning.se>
2021-02-16 09:48:14 +0100

    lektion 5

---
+++ Lektion-5/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
+
+
+
+module.exports = app;
