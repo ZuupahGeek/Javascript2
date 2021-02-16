4c8a636765a64469cedb1fb3fff161ec350a24a0
Joachim Karlsson <joachim.karlsson@utb.ecutbildning.se>
2021-02-16 11:49:55 +0100

    lektion 5

--- Lektion-5/app.js
+++ Lektion-5/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-
+const productController = require('./controllers/productController');
 
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use('/api/products', productController)
 
 
 
---
+++ Lektion-5/controllers/productController.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const productModel = require('../models/products/productModel');
+
+
+router.get('/', productModel.getProducts);
+router.get('/:id', productModel.getProduct);
+
+router.post('/new', productModel.createProduct);
+
+router.patch('/:id', productModel.updateProduct)
+
+
+module.exports = router;
